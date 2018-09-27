f19c606fb806534dfe3bacb36a774d0fe8eb7cb6
Emiliano Carrillo Moncayo <EmilianoCarrilloMoncayo@gmail.com>
2018-09-27 00:04:16 -0500

    Add files via upload

---
+++ recursos.php
@@ -0,0 +1,279 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+    <title>FEPRO | Feria de Proyectos 2017 ` Recursos</title>
+    <meta name="description" content="La FEPRO promueve la generación de talento en las áreas de Tecnologías de la Información, con un compromiso social o científico, y contribuye al desarrollo de habilidades técnicas y de innovación en los estudiantes universitarios">
+    <meta name="keywords" content="FERPRO, BUAP, Facultad de Ciencias de la Computacion, Puebla, Tecnologias, Feria de Proyectos, Informatica, Software, Mexico"/>
+
+    <meta name="msapplication-TileColor" content="#8ac329">
+    <meta name="msapplication-TileImage" content="fepro.cs.buap.mx">
+    <meta name="theme-color" content="#8ac329">
+
+    <!-- icons & favicons -->
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />  <!-- this icon shows in browser toolbar -->
+    <link rel="icon" type="image/x-icon" href="favicon.ico" /> <!-- this icon shows in browser toolbar -->
+
+<!-- Fallback For IE 9 [ Media Query and html5 Shim] -->
+<!-- use these two below scripts if you want to load from cloud -->
+<!--script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script-->
+<!--script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script-->
+<!--[if lt IE 9]>
+<script src="assets/vendor/css3-mediaqueries/css3-mediaqueries.min.js"></script>
+<script src="assets/vendor/html5shiv/html5shiv.min.js"></script>
+<script src="assets/vendor/html5shiv/html5shiv-printshiv.min.js"></script>
+<![endif]-->
+
+
+<!-- BOOTSTRAP CSS -->
+<!-- bootstrap can be added from local or cdn, bellow line is for local, btw, there is js which is added in footer -->
+<link rel="stylesheet" href="assets/vendor/bootstrap/css/bootstrap.min.css" media="all" />
+<!-- to add bootstrap from cdn pls uncomment the bellow line -->
+<!--link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"-->
+
+<!-- FONT AWESOME CSS -->
+<!-- font awesome css library can be can be added from local or cdn, bellow line is for local -->
+<link rel="stylesheet" href="assets/vendor/fontawesome/css/font-awesome.min.css" media="all" />
+<!-- to add font awesome from cdn pls uncomment the bellow line -->
+<!--link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"-->
+
+<!-- for animation effects animate.css -->
+<link rel="stylesheet" href="assets/vendor/animate/animate.min.css" media="all" />
+
+<!-- adding maginific popup library, there is js which is added in footer -->
+<link rel="stylesheet" href="assets/vendor/maginificpopup/magnific-popup.min.css"  media="all" />
+
+<!-- adding google fonts -->
+<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700%7cExo:300,600,700,900"/>
+
+<!-- CUSTOM  CSS  -->
+<link id="cbx-style" rel="stylesheet" href="assets/css/style-default.min.css" media="all" />
+<link id="cbx-customstyle" rel="stylesheet" href="assets/css/custom.css" media="all" />
+
+<!-- MODERNIZER CSS  -->
+<script src="assets/vendor/modernizer/modernizr.js"></script>
+
+</head>
+
+<body>
+
+<!--[if lt IE 7]>
+<p class="browsehappy">We are Extreamly sorry, But the browser you are using is probably from when civilization started.
+    Which is way behind to view this site properly. Please update to a modern browser, At least a real browser. </p>
+<![endif]-->
+
+<div class="cbx-container">
+
+    <header>
+        <div id="cbx-header" class="cbx-header">
+
+            <div class="cbx-header-inner">
+
+               <nav id="menu-offscroll" class="navbar navbar-default cbx-navbar">
+                    <div class="container">
+                        <div class="row">
+
+                            <div class="col-sm-2">
+                                <!-- Brand and toggle get grouped for better mobile display -->
+                                <div class="navbar-header">
+                                    <button type="button" class="navbar-toggle collapsed " data-toggle="collapse"
+                                            data-target="#navbar-collapse">
+                                        <span class="sr-only">Toggle navigation</span>
+                                        <span class="icon-bar"></span>
+                                        <span class="icon-bar"></span>
+                                        <span class="icon-bar"></span>
+                                    </button>
+
+                                    <div class="logo">
+                                        <h2><a href="index.php" class="gotome">FEPRO<span
+                                                class="span-color">2017</span></a></h2>
+                                    </div>
+
+                                </div>
+                            </div> <!--//.COL 3-->
+
+                            <div class="col-sm-10">
+                                <!-- Collect the nav links, forms, and other content for toggling -->
+                                <div class="collapse navbar-collapse" id="navbar-collapse">
+                                    <ul class="nav navbar-nav cbx-nav-right xs-text-center">
+                                        <li><a href="index.php">¿Qué es FEPRO?</a></li>
+                                        <li><a href="index.php">Convocatoria</a></li>
+                                        <li><a href="index.php">Categorias e Impactos</a></li>
+                                        <!-- <li><a href="#cbx-schedule" class="gotome">Agenda</a></li>
+                                        <li><a href="#cbx-travelinfo" class="gotome">Ganadores</a></li>-->
+                                        <li><a href="index.php">Prensa</a></li>
+                                        <li><a href="recursos.php">Recursos</a></li>
+                                        
+                                        <!--<li><a href="#cbx-register" class="gotome">Recursos</a></li>-->
+                                    </ul>
+                                    <ul class="list-inline pull-right event-ticket">
+                                        <li><a href="registro.php">¡Inscribete ahora!</a></li>
+                                    </ul>
+                                </div><!-- /.navbar-collapse -->
+                            </div> <!--//.COl 9-->
+
+                        </div><!--//.ROW-->
+                    </div><!-- /.container -->
+                </nav>
+
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <div class="cbx-header-content text-center cbx-regiter-common">
+                                <h4 class="wow zoomIn " data-wow-delay=".1s">¡10 años Rompiendo Paradigmas!</h4>
+                                <h1 class="uppercase  wow zoomIn " data-wow-delay=".3s">Feria<span
+                                        class="span-color2">  de Proyectos</span></h1>
+                                <h5 class="uppercase  wow zoomIn " data-wow-delay=".5s"><span class="uppercase">11 y 12 Septiembre 2017,</span>
+                                    <span class="sspan-color2"> Complejo Cultural Universitario BUAP</span></h5>
+                            </div>
+                        </div>
+                    </div> <!--.content ROW-->
+
+                </div><!--//.CONTAINER-->
+            </div>
+        </div> <!--//.CBX FOOTER-->
+    </header>
+
+
+    <!--SPEAKERS START-->
+
+    <div id="intro" class="cbx-header">
+        <div class="" style="margin-top:15px;">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12 col-md-12 text-center">
+                        <div class="cbx-heading">
+                            <h2 class="uppercase wow fadeInUp " data-wow-delay=".1s">
+                                    <span class="heading-serial">
+                                        <span class="heading-serial-inner">
+                                          01
+                                        </span>
+                                    </span>
+                                <span class="title">Recursos</span>
+                            </h2>
+
+                            <p class="uppercase  wow fadeInUp" data-wow-delay=".2s">
+                                Ponemos a tu disposición diversos recursos para que puedas utilizarlos.
+                            </p>
+                            <br>
+                            <div class="row">
+                                <div class="col-md-3">
+                                    <h3>CV FEPRO 2017</h3>
+                                    <p style="margin-top:30px;"><a href="recursos/cv_fepro.docx" class="boton">Descargar</a></p>
+                                </div>
+                                <div class="col-md-3">
+                                    <h3>FORMATO DE ARTÍCULO</h3>
+                                    <p style="margin-top:30px;"><a href="recursos/articulos_IEEE.docx" class="boton">Descargar</a></p>
+                                </div>
+                                <div class="col-md-3">
+                                    <h3>FORMATO CARTEL IEEE</h3>
+                                    <p style="margin-top:30px;"><a href="recursos/cartel_ieee.pdf" download class="boton">Descargar</a></p>
+                                </div>
+                                <div class="col-md-3">
+                                    <h3>LOGOS OFICIALES</h3>
+                                    <p style="margin-top:30px;"><a href="recursos/logos.zip" class="boton">Descargar</a></p>
+                                </div>
+                            </div> 
+                        </div>
+                        <img src="assets/img/logo.png" alt="Logo FPRO" style="width:250px">
+                    </div> <!--//main COL-->
+                </div> <!--//main row-->
+
+            </div> <!--//container-->
+
+        </div>
+    </div>
+
+</div>
+
+    <!--SUBSCRIBER START-->
+
+    <div id="cbx-subscriber" class="cbx-subscriber">
+        <div class="cbx-testi-inner">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12 text-center">
+                        <h3 class=" wow fadeInUp" data-wow-delay=".1s">Síguenos en redes sociales</h3>
+                    </div> <!--//main COL-->
+                </div> <!--// row-->
+            
+            </div> <!--//container-->
+        </div> <!--//cbx-inner-->
+    </div>
+    <!--CONTACT END-->
+
+    <footer>
+    <div id="cbx-footer" class="cbx-footer">
+        <div class="cbx-footer-inner cbx-top-inner">
+            <div class="container">
+                <div class="footer-content">
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-12 text-center">
+                            <div class="footer-social wow zoomIn" data-wow-delay=".5s">
+                                <ul class="list-unstyled list-inline text-center">
+                                    <li><a class="sp-fb" href="https://www.facebook.com/feprofcc/" target="_blank"><i class="fa fa-facebook"></i></a></li>
+                                    <li><a class="sp-tw" href="https://twitter.com/FePro_" target="_blank"><i class="fa fa-twitter"></i></a></li>
+
+                                    <li><a class="sp-in" href="https://www.instagram.com/fepro_2017/" target="_blank"><i class="fa fa-instagram"></i></a></li>
+                                    <li><a class="sp-in" href="https://www.youtube.com/user/FeProBUAP" target="_blank"><i class="fa fa-youtube"></i></a></li>
+                                </ul>
+                            </div>
+                            <p style="color:#fff;" class="cbx-copy-right uppercase wow fadeInUp" data-wow-delay=".6s"><span class="span-color2 cbx-copy-symbol">&copy;</span> Copyright 2017. <span class="span-color1"><a href="http://www.cs.buap.mx/" style="color:#fff;">Facultad de Ciencias de Computación - BUAP</a>. </span>All Right Reserved.</p>
+                        </div>
+                    </div>
+                </div>
+            </div><!--//.CONTAINER-->
+            <div class="cbx-top text-center">
+                <a href="#cbx-header" class="gotome">
+                    <i class="fa fa-angle-double-up"></i>
+                    <p></p>
+                </a>
+            </div>
+        </div> <!--//.cbx-inner-->
+    </div> <!--//.CBX FOOTER-->
+</footer>
+
+
+    <!-- SITE CONTENT END -->
+</div> <!--//.CBX CONTAINER-->
+
+
+<!-- SITE  SCRIPT  -->
+<script src="assets/vendor/jquery/jquery-3.1.1.min.js"></script>
+<!-- BOOTSTRAP JS -->
+<!-- adding bootstrap js -->
+<script src="assets/vendor/bootstrap/js/bootstrap.min.js"></script>  <!-- from local -->
+<!-- load bootstrap js from cdn, uncomment bellow line if need from cdn -->
+<!--script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script-->
+
+
+<!-- WOW  JS -->
+<script src="assets/vendor/wow/wow.min.js"></script>
+
+<!--- for smooth scroll -->
+<script src="assets/vendor/jquery-smooth-scroll/jquery.smooth-scroll.min.js"></script>
+
+
+<!-- adding magnific popup js library -->
+<script src="assets/vendor/maginificpopup/jquery.magnific-popup.min.js" type="text/javascript"></script>
+
+<script src="assets/vendor/validation/jquery.validate.min.js"></script>
+
+<!-- if use countdown plugin then use this plugin -->
+<script type="text/javascript" src="assets/vendor/countdown/kinetic.min.js"></script>
+<script type="text/javascript" src="assets/vendor/countdown/jquery.final-countdown.min.js"></script>
+
+<!-- CUSTOM JS -->
+<script src="assets/js/theme.min.js"></script>
+<script src="assets/js/custom.js"></script>
+
+
+</body>
+
+</html>
+
+
+
